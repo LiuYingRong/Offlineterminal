412df49f54e51b80f912ee893c04082f3ab7fb59
liuyingrong <liuting126@qq.com>
2020-10-29 15:18:38 +0800

    1. 新增提交文档的更新日期功能 2. 新增评论功能 3. 新增知识库模块

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,8 @@
 module.exports = {
     title: '外业离线端',
+    head: [
+        ['link', { rel: 'shortcut icon', href: '/favorite.icon', type: 'image/jpg' }]
+    ],
     description: '外业离线端开发文档',
     port: 6066,
     base: '/Offlineterminal/',
@@ -7,9 +10,34 @@ module.exports = {
         toc: { includeLevel: [1, 2, 3] },
         lineNumbers: true
     },
-    plugins: ['@vuepress/back-to-top'],
+    plugins: [
+        ['@vuepress/back-to-top'],
+        [
+            '@vuepress/last-updated',
+            {
+                transformer: (timestamp) => {
+                    const dayjs = require('dayjs')
+                    return dayjs(timestamp).format('YYYY-MM-DD HH:mm:ss')
+                }
+            }
+        ],
+        [
+            '@vssue/vuepress-plugin-vssue', {
+                platform: 'github-v4',
+                locale: 'zh',
+                // 其他的 Vssue 配置
+                owner: 'LiuYingRong', //github账户名
+                repo: 'Offlineterminal', //github项目的名称
+                clientId: 'f998dc6362cf14b8c23a',
+                clientSecret: '60941b7d722df940e5b2b9803fc9fbb88704d5d5',
+                autoCreateIssue: true // 自动创建评论，默认是false
+            },
+        ]
+    ],
     // 主题配置
     themeConfig: {
+        // 显示更新时间
+        lastUpdated: '上次更新',
         // 网站logo
         logo: '/logo.png',
         // 顶部导航栏
@@ -33,10 +61,11 @@ module.exports = {
                 ['/guide/UpdateLog', '更新日志'],
             ]
         }, {
-            title: "深入",
+            title: "进阶",
             collapsable: false,
             children: [
                 ['/guide/Api', 'Api接口文档'],
+                ['/guide/KnowledgeBase', '知识库'],
             ]
         }, ]
     }
